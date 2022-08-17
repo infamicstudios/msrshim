60a7693f911a6f8533c1a503780a68dac397cb9b
Duncan <duncanmcfarlane@yahoo.com>
2022-08-17 13:32:15 -0700

    Implemented better allowlist searching and storage of bitmasks

--- msrgenerate.sh
+++ msrgenerate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Request a file to write results to
-echo logfile name? \(should be a .txt\): 
+echo logfile name? \(should be a .h\): 
 read logFile
 
 ###
@@ -13,19 +13,25 @@ RANGEHIGH=8192 # Covers entire range of possible x86 MSRs
 ALLOWEDMSRS="allowlist.txt"
 
 echo Generating MSRS for range ${RANGELOW} to ${RANGEHIGH} and printing results to ${logFile}
+#Insert a header into the output file for readability
+echo // FORMAT: MSR Value, If it exists, contained within allowlist, allowlist mask >> ${logFile}
+
 
 for ((i=RANGELOW;i<=RANGEHIGH;i++)) do 
     addr=$(printf "%#x\n" $i) # Convert to hex
     regresult=`rdmsr ${addr} 2> /dev/null` # Filter unadressable MSRs
    if [ -z "$regresult" ]; then
-       echo \{NULL, false, false\}, >> ${logFile}
+       echo \{NULL, false, false, NULL\}, >> ${logFile}
    else # Register is available
        addr=$(printf "%#010x" $i) # Convert to the format of hex in the allowfile
-       # Search for the MSR in the allowfile this can be made arbitrarily more complicated through regex patterns.
-       if grep -q ${addr} ${ALLOWEDMSRS}; then
-          echo \{0x${regresult}ULL, true, true\}, >> ${logFile}
+       # This is horribly inefficent and needs to be refactored but it searchs the first
+       # Column of allowlist for matchs to addr.
+       if cat ${ALLOWEDMSRS} | awk '{ print $1 }' | grep -q ${addr}; then
+          # Then extracts the bitmask 
+          mask=$( cat ${ALLOWEDMSRS} | grep ${addr} | awk '{ print $2 }' )
+          echo \{0x${regresult}ULL, true, true, ${mask}\}, >> ${logFile}
        else
-          echo \{0x${regresult}ULL, true, false\}, >> ${logFile}
+          echo \{0x${regresult}ULL, true, false, NULL\}, >> ${logFile}
        fi
     fi
 done
