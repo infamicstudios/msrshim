f4377cd697cfd16e17013de78d4d8aebc1592dd5
Duncan <duncanmcfarlane@yahoo.com>
2022-08-17 11:53:47 -0700

    Finally fixed msrgenerategit add msrgenerate.sh

--- msrgenerate.sh
+++ msrgenerate.sh
@@ -12,6 +12,8 @@ RANGELOW=0
 RANGEHIGH=8192 # Covers entire range of possible x86 MSRs
 ALLOWEDMSRS="allowlist.txt"
 
+echo Generating MSRS for range ${RANGELOW} to ${RANGEHIGH} and printing results to ${logFile}
+
 for ((i=RANGELOW;i<=RANGEHIGH;i++)) do 
     addr=$(printf "%#x\n" $i) # Convert to hex
     regresult=`rdmsr ${addr} 2> /dev/null` # Filter unadressable MSRs
@@ -20,7 +22,7 @@ for ((i=RANGELOW;i<=RANGEHIGH;i++)) do
    else # Register is available
        addr=$(printf "%#010x" $i) # Convert to the format of hex in the allowfile
        # Search for the MSR in the allowfile this can be made arbitrarily more complicated through regex patterns.
-       if grep -Fxq ${addr} ${ALLOWEDMSRS}; then
+       if grep -q ${addr} ${ALLOWEDMSRS}; then
           echo \{0x${regresult}ULL, true, true\}, >> ${logFile}
        else
           echo \{0x${regresult}ULL, true, false\}, >> ${logFile}
